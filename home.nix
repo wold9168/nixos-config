154227c389d28ae2112805cef3f33ad3fdf23c31
wold9168 <moewold@outlook.com>
2026-06-30 03:12:25 +0800

    initial commit
    Signed-off-by: wold9168 <moewold@outlook.com>

---
+++ home.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ...}:
+{
+	home.username = "wold9168";
+	home.homeDirectory = "/home/wold9168";
+	programs.git = {
+		enable = true;
+		userName = "wold9168";
+		userEmail = "moewold@outlook.com";
+	};
+	
+	# User-level Applications
+	home.packages = with pkgs;[
+		# compress
+		zip
+		xz
+		unzip
+		p7zip
+		gnutar
+		zstd
+
+		# performance
+		btop
+		htop
+		iotop
+		iftop
+
+		# utils
+		ripgrep
+		jq
+		fd
+		gawk
+		gnupg
+		jekyll
+		glow
+
+		# nix-related
+		nix-output-monitor
+
+		# misc
+		which
+		file		
+	];
+	programs.starship = {
+		enable = true;
+		settings = {
+			add_newline = false;
+			aws.disabled = true;
+			gcloud.disabled = true;
+			line_break.disabled = true;
+		};
+	};
+	
+	home.stateVersion = "26.05";
+}
