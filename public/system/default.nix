c948008200d2e534ed002a489ee3fabca22da03b
wold9168 <moewold@outlook.com>
2026-07-13 04:43:55 +0800

    refactor: extract i18n and nix-feature config from default.nix into separate files
    Signed-off-by: wold9168 <moewold@outlook.com>

--- public/system/default.nix
+++ public/system/default.nix
@@ -4,25 +4,7 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "zh_CN.UTF-8";
-    LC_IDENTIFICATION = "zh_CN.UTF-8";
-    LC_MEASUREMENT = "zh_CN.UTF-8";
-    LC_MONETARY = "zh_CN.UTF-8";
-    LC_NAME = "zh_CN.UTF-8";
-    LC_NUMERIC = "zh_CN.UTF-8";
-    LC_PAPER = "zh_CN.UTF-8";
-    LC_TELEPHONE = "zh_CN.UTF-8";
-    LC_TIME = "zh_CN.UTF-8";
-  };
-
-  imports = [ ./networking.nix ];
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  imports = [ ./networking.nix ./i18n.nix ./nix-feature.nix ];
   
   environment.systemPackages = import ./system-level-pkgs.nix { inherit inargs; };
 
