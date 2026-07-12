ad3ded1312373e2d56af979cfc18a9aed981680e
wold9168 <moewold@outlook.com>
2026-07-13 06:37:01 +0800

    Add TODO for zsh
    Signed-off-by: wold9168 <moewold@outlook.com>

--- home/wold9168/base/zsh/default.nix
+++ home/wold9168/base/zsh/default.nix
@@ -19,5 +19,9 @@
     enable = true;
     enableZshIntegration = true;
   };
+  
+  # TODO: Set the prompt as $
 
+  # TODO: Try to set up oh-my-zsh and powerlevel10k
+  # https://search.nixos.org/packages?channel=26.05&query=powerlevel&size=30#show=zsh-powerlevel10k
 }
