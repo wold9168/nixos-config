b1f03aa579505680667374ed14daf59d0d93492e
wold9168 <moewold@outlook.com>
2026-07-13 05:38:04 +0800

    refactor: remove starship config from home-manager
    Signed-off-by: wold9168 <moewold@outlook.com>

--- home/wold9168/default.nix
+++ home/wold9168/default.nix
@@ -40,15 +40,6 @@
     which
     file
   ];
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
 
   home.stateVersion = "26.05";
 }
