57486b05a598f8418650d6638b7a5bfc3c48c680
wold9168 <moewold@outlook.com>
2026-07-14 03:47:32 +0800

    Enable fcitx5
    Signed-off-by: wold9168 <moewold@outlook.com>

---
+++ home/wold9168/graphic/im/fcitx5-rime/default.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+{
+  # catppuccin.fcitx5.enable = false;
+  # xdg.configFile = {
+  #   "fcitx5/profile" = {
+  #     source = ./profile;
+  #     # every time fcitx5 switch input method, it will modify ~/.config/fcitx5/profile,
+  #     # so we need to force replace it in every rebuild to avoid file conflict.
+  #     force = true;
+  #   };
+  #   # "mozc/config1.db".source =
+  #   #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/home/linux/gui/base/fcitx5/mozc-config1.db";
+  # };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.waylandFrontend = true;
+    fcitx5.addons = with pkgs; [
+      qt6Packages.fcitx5-configtool # GUI for fcitx5
+      fcitx5-gtk # gtk im module
+
+      # Chinese
+      fcitx5-rime # for flypy chinese input method
+      # fcitx5-chinese-addons # we use rime instead
+
+      # Japanese
+      # ctrl-i / F7 - convert to takakana
+      # ctrl-u / F6 - convert to hiragana
+      # fcitx5-mozc-ut # Moze with UT dictionary
+      fcitx5-anthy
+
+      # Korean
+      # fcitx5-hangul
+    ];
+  };
+}
