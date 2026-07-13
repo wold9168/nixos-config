5ebd5ae8451db0ce65bf5b7d1f59dc47f44d0aed
wold9168 <moewold@outlook.com>
2026-07-14 03:11:38 +0800

    Add ssh key
    Signed-off-by: wold9168 <moewold@outlook.com>

--- var/default.nix
+++ var/default.nix
@@ -3,8 +3,10 @@
   username = "wold9168";
   userfullname = "Shinonome Tera tanuki";
   useremail = "moewold@outlook.com";
-  mainSshAuthorizedKeys = [];
-  secondaryAuthorizedKeys = [];
+  mainSshAuthorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPpRSgZTTzpWvrLMPceSkrkDe2LtHumAWd/33p+ExU9G moewold@outlook.com"
+  ];
+  secondaryAuthorizedKeys = [ ];
   networking = import ./networking.nix { inherit lib; };
   mainGpgAuthorizedKey = "FC943B58B2F66D8C";
-} 
+}
