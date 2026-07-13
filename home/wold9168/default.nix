7db911c4a2f275f34bf8754ee558ea167ae98d31
wold9168 <moewold@outlook.com>
2026-07-13 17:40:52 +0800

    refactor: add scanPaths support to home-manager config
    Signed-off-by: wold9168 <moewold@outlook.com>

--- home/wold9168/default.nix
+++ home/wold9168/default.nix
@@ -1,13 +1,17 @@
-{ pkgs, myvar, ... }:
 {
-  imports = [ ./base ];
+  pkgs,
+  myvar,
+  mylib,
+  ...
+}:
+{
+  imports = [ ./base ] ++ mylib.scanPaths ./.;
+  
   home.username = myvar.username;
   home.homeDirectory = "/home/wold9168";
 
   # User-level Applications
-  home.packages = with pkgs; [
-
-  ];
+  home.packages = with pkgs; [ ];
 
   home.stateVersion = "26.05";
 }
