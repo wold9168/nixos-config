46efaa8fa616d346697df31dc5e7ab2d83af3e0c
wold9168 <moewold@outlook.com>
2026-07-15 04:21:14 +0800

    disable oh-my-posh
    Signed-off-by: wold9168 <moewold@outlook.com>

--- home/wold9168/headless/base/zsh/default.nix
+++ home/wold9168/headless/base/zsh/default.nix
@@ -14,15 +14,13 @@
       append = true;
       extended = true;
     };
+    ohMyZsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "z"
+      ];
+      theme = "robbyrussell";
+    };
   };
-
-  programs.oh-my-posh = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  # TODO: Set the prompt as $
-
-  # TODO: Try to set up oh-my-zsh and powerlevel10k
-  # https://search.nixos.org/packages?channel=26.05&query=powerlevel&size=30#show=zsh-powerlevel10k
 }
