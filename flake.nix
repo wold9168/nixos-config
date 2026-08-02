a277a9aa13fb85bd457f6e85bfd18eacad18c67d
wold9168 <Shinonome Tera tanuki>
2026-08-02 23:19:22 +0800

    Add cachix substituters server
    Signed-off-by: wold9168 <Shinonome Tera tanuki>

--- flake.nix
+++ flake.nix
@@ -5,10 +5,12 @@
   nixConfig = {
     # substituers will be appended to the default substituters when fetching packages
     extra-substituters = [
+      "https://nix-community.cachix.org"
       "https://cache.nixos-cuda.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
